feeae77069dd97140f9abafe11300e815f0593c0
庞淙心 <14425657+pang-congxin@user.noreply.gitee.com>
2025-07-02 09:03:43 +0800

    修改会议启动类和添加数据库包

--- src/main/java/com/cemenghui/meeting/DemoApplication.java
+++ src/main/java/com/cemenghui/meeting/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.example.demo.dao")
+@MapperScan(basePackages = "com.cemenghui.meeting.dao")
 public class DemoApplication {
 
     public static void main(String[] args) {
