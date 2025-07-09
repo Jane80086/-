4af18347d49da11a40faed2f76c46357cc2dd3eb
wakeup-wakeup <2561814198@qq.com>
2025-07-10 00:03:19 +0800

    恢复login接口

--- system/src/main/java/com/cemenghui/system/controller/AuthController.java
+++ system/src/main/java/com/cemenghui/system/controller/AuthController.java
@@ -1,10 +1,11 @@
 package com.cemenghui.system.controller;
 
+import com.cemenghui.system.dto.LoginRequestDTO;
+import com.cemenghui.system.dto.LoginResponseDTO;
 import com.cemenghui.system.util.CaptchaUtil;
+import com.cemenghui.system.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +22,10 @@ public class AuthController {
 
     @Autowired
     private RedisUtil redisUtil;
+
+    @Autowired
+
+    private com.cemenghui.system.service.LoginService loginService;
 
     @GetMapping("/captcha")
     public void getCaptcha(HttpServletResponse response, @RequestParam("uuid") String uuid) throws IOException {
@@ -41,4 +46,19 @@ public class AuthController {
         response.setContentType("image/png");
         ImageIO.write(image, "png", response.getOutputStream());
     }
-} 
+
+    /**
+
+     * 登录接口
+
+     */
+
+    @PostMapping("/login")
+
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO request) {
+
+        return loginService.login(request.getAccount(), request.getPassword());
+
+    }
+
+}
